5b47d570761773d2d4c74f419512e75a4c212ca9
toshiki <toshiki.u.19@gmail.com>
2023-09-10 23:11:53 +0900

    保存した動画の解析

--- openpose_webapp/app.py
+++ openpose_webapp/app.py
@@ -1,73 +1,79 @@
-# app.py
-
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import os
-import cv2
-import numpy as np
-from openpose import pyopenpose as op
+import subprocess
+import json
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
 
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = 'uploads'
+JSON_OUTPUT_FOLDER = 'json_output'
+VIDEO_OUTPUT_FOLDER = 'video_output'
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov'}
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['JSON_OUTPUT_FOLDER'] = JSON_OUTPUT_FOLDER
+app.config['VIDEO_OUTPUT_FOLDER'] = VIDEO_OUTPUT_FOLDER
 
-# OpenPoseの設定
-params = dict()
-params["model_folder"] = "path_to_openpose/models"  # OpenPoseのモデルフォルダへのパス
-params["net_resolution"] = "320x240"  # 解像度の設定
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
-opWrapper = op.WrapperPython()
-opWrapper.configure(params)
-opWrapper.start()
-
-def analyze_video(video_path):
-    datum = op.Datum()
-
-    # 動画を読み込み
-    cap = cv2.VideoCapture(video_path)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640, 480))
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # OpenPoseでフレームを解析
-        datum.cvInputData = frame
-        opWrapper.emplaceAndPop([datum])
-
-        # 解析結果を取得
-        pose_keypoints = datum.poseKeypoints
-
-        # 解析結果をフレームに描画（ここで解析結果をフレームに描画する処理を実装）
-
-        # 動画にフレームを書き込み
-        out.write(frame)
-
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], 'input_video.mp4')
-        file.save(filename)
-        analyze_video(filename)
-        return render_template('result.html')
+def upload():
+    #if 'video' not in request.files:
+        #return redirect(request.url)
+    
+    #video_file = request.files['video']
+    #if video_file.filename == '':
+        #return redirect(request.url)
+    
+    #if video_file and allowed_file(video_file.filename):
+        # アップロードされた動画を保存
+        # video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_file.filename)
+        # video_path = os.path.join(app.config['UPLOAD_FOLDER'], 'video.avi')
+        video_path = "C:\\Users\\toshi\\OneDrive\\ドキュメント\\myjlab\\openpose_webapp\\uploads\\video.avi" # ここで video_path を定義する
+        #video_file.save(video_path)
+
+        output_directory = os.path.join(app.config['JSON_OUTPUT_FOLDER'], 'output')
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+    
+        json_output_path = os.path.join(output_directory, 'output.json')
+        video_output_path = os.path.join(app.config['VIDEO_OUTPUT_FOLDER'], 'output.mp4')
+
+        # カレントディレクトリをOpenPoseのディレクトリに変更
+        openpose_directory = r'C:\\openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended\\openpose'
+        os.chdir(openpose_directory)
+        
+        # OpenPoseの実行コマンド（パスはエスケープ文字を含む）
+        #openpose_command = f'bin\\OpenPoseDemo.exe --video "{video_path}" --write_json "{json_output_path}" --write_video "{video_output_path}" .'
+        openpose_command = f'bin\\OpenPoseDemo.exe --video "{video_path}" --write_json "{output_directory}" .'
+        
+        # コマンドを実行
+        print("Executing OpenPose command:", openpose_command)
+        subprocess.run(openpose_command, shell=True)
+        
+        return redirect(url_for('result', video_filename='output.mp4', json_filename='output.json'))
+    
+@app.route('/result')
+def result():
+    video_filename = request.args.get('video_filename', '')
+    json_filename = request.args.get('json_filename', '')
+    
+    # JSONデータを読み込む
+    json_data = {}
+    json_path = os.path.join(app.config['JSON_OUTPUT_FOLDER'], json_filename)
+    if os.path.exists(json_path):
+        with open(json_path, 'r') as json_file:
+            json_data = json.load(json_file)
+    
+    return render_template('result.html', video_filename=video_filename, json_data=json_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
