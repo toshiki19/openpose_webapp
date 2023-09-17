15d41c4340e2d46ccb66d24be71408412308dd2f
toshiki <toshiki.u.19@gmail.com>
2023-09-17 14:16:14 +0900

    openpose_weappにjsonファイルの書き出し成功

--- app.py
+++ app.py
@@ -50,57 +50,40 @@ def upload():
             video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_filename)
             file.save(video_path)
 
-        # 以降の処理で video_path を使用できます
+            # カレントディレクトリをOpenPoseのディレクトリに変更
+            openpose_directory = r'C:\\openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended\\openpose'
+            os.chdir(openpose_directory)
+
+            # JSON出力の保存先を設定
+            json_output_directory = os.path.join(app.config['JSON_OUTPUT_FOLDER'], 'output')
+            if not os.path.exists(json_output_directory):
+                os.makedirs(json_output_directory)
+
+            # JSON出力ファイルのパスを設定
+            json_output_path = os.path.join(json_output_directory, 'output.json')
+
+            # OpenPoseの実行コマンド
+            openpose_command = f'bin\\OpenPoseDemo.exe --video "{video_path}" --write_json "{json_output_directory}" .'
+
+            # コマンドを実行
+            print("Executing OpenPose command:", openpose_command)
+            subprocess.run(openpose_command, shell=True)
+
+            # カレントディレクトリを元のディレクトリに戻す
+            os.chdir(app.root_path)
+
+            return redirect(url_for('result', json_filename='output.json'))
 
         else:
             flash('Invalid file type')
             return redirect(request.url)
-        
-        # video_path = "C:\\Users\\toshi\\OneDrive\\ドキュメント\\myjlab\\openpose_webapp\\uploads\\video.avi" # ここで video_path を定義す
-
-            # カレントディレクトリをOpenPoseのディレクトリに変更
-        openpose_directory = r'C:\\openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended\\openpose'
-        os.chdir(openpose_directory)
 
 
-        # JSON出力の保存先を設定
-        json_output_directory = os.path.join(app.config['JSON_OUTPUT_FOLDER'], 'output')
-        if not os.path.exists(json_output_directory):
-            os.makedirs(json_output_directory)
-        
-        # JSON出力ファイルのパスを設定
-        json_output_path = os.path.join(json_output_directory, 'output.json')
-    
-        # OpenPoseの実行コマンド
-        openpose_command = f'bin\\OpenPoseDemo.exe --video "{video_path}" --write_json "{json_output_directory}" .'
-    
-        # コマンドを実行
-        print("Executing OpenPose command:", openpose_command)
-        subprocess.run(openpose_command, shell=True)
-
-        return redirect(url_for('result', video_filename='output.mp4', json_filename='output.json'))
-
-       
-    
 @app.route('/result')
 def result():
-    # video_filename = request.args.get('video_filename', '')
-    # json_filename = request.args.get('json_filename', '')
-    
-    # # JSONデータを読み込む
-    # json_data = {}
-    # json_path = os.path.join(app.config['JSON_OUTPUT_FOLDER'], json_filename)
-    # if os.path.exists(json_path):
-    #     with open(json_path, 'r') as json_file:
-    #         json_data = json.load(json_file)
-    
-    #     # 動画ファイルのパスを設定
-    # video_path = os.path.join(app.config['VIDEO_OUTPUT_FOLDER'], video_filename)
-
-    # return render_template('result.html', video_filename=video_filename, json_data=json_data)
+    json_filename = request.args.get('json_filename', '')
 
     # JSONデータのパス
-    json_filename = 'output.json'
     json_path = os.path.join(app.config['JSON_OUTPUT_FOLDER'], json_filename)
 
     return render_template('result.html', json_path=json_path)
